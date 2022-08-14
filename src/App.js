79845088f4c9377ddfe810bfb133c535f733cb52
ryuken73 <ryuken73@gmail.com>
2022-08-14 23:41:52 +0900

    refactor: useAutoPlay

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ConfigDialog from './ConfigDialog';
 import Box from '@mui/material/Box';
 import { useHotkeys } from 'react-hotkeys-hook';
 import useLocalStorage from './hooks/useLocalStorage';
+import useAutoPlay from './hooks/useAutoPlay';
  
 const mirrorModalPlayer = (playerNode, modalPlayer) => {
   const videoElement =  playerNode.querySelector('video');
@@ -75,23 +76,7 @@ function App() {
     autoPlayIndexRef.current = gridNum;
   },[modalPlayer, gridDimension, cctvsSelectedArray, enableOverlayGlobal])
 
-    React.useEffect(() => {
-    let timer;
-    if(autoPlay){
-      document.title=`CCTV[auto - every ${autoInterval}s]`
-      const firstIndex = autoPlayIndexRef.current;
-      maximizeGrid(firstIndex);
-      timer = setInterval(() => {
-        const nextIndex = (autoPlayIndexRef.current + 1) % 9;
-        maximizeGrid(nextIndex);
-      },autoInterval*1000)
-    } else {
-      document.title="CCTV"
-    }
-    return () => {
-      if(timer) clearInterval(timer);
-    }
-  },[autoPlay, maximizeGrid])
+  useAutoPlay({autoPlay, autoInterval, maximizeGrid, autoPlayIndexRef});
 
   const toggleAutoPlay = React.useCallback(() => {
     setAutoPlay(autoPlay => {
