290fa87167fc6d22be4a9930f514a65abb5c60c5
ryuken73 <ryuken73@gmail.com>
2023-05-18 14:41:18 +0900

    update: get youtube hls url (the server is running on sslproxydev server)

--- src/lib/sourceUtil.js
+++ src/lib/sourceUtil.js
@@ -1,3 +1,7 @@
+import config from '../config';
+const {YOUTUBE_HLS_URL_GET_API} = config;
+
+
 export const setUniqAreasFromSources = (cctvs, setFunction) => {
     const areasOnly = cctvs.map(cctv => {
         return cctv.title.split(' ')[0]
@@ -24,4 +28,26 @@ export const orderByArea = cctvs => {
     const orderedByAreaMap = groupCCTVsByArea(uniqAreas, cctvs);
     return [...orderedByAreaMap.values()].flat();
 }  
+
+export const getYoutubePlaylistUrl = videoId => {
+    return new Promise((resolve, reject) => {
+        fetch(`${YOUTUBE_HLS_URL_GET_API}/${videoId}`)
+        .then(response => response.json())
+        .then(result => {
+            if(result.success){
+                resolve(result.result)
+            } else {
+                throw new Error(result.error)
+            }
+        })
+        .catch(err => {
+            reject(err);
+        })
+    })
+}
+
+export const getYoutubeId = url => {
+    const addr = new URL(url);
+    return addr.searchParams.get('v');
+}
     
